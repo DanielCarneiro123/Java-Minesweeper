e41d36aaf2e38c7e36bdbf3c51f03912f3d683ad
DanielCarneiro123 <92723426+DanielCarneiro123@users.noreply.github.com>
2022-12-08 23:16:47 +0000

    Delete hero-solid-master directory

--- hero-solid-master/src/main/java/com/aor/hero/model/game/elements/Cell.java
+++
@@ -1,8 +0,0 @@
-package com.aor.hero.model.game.elements;
-
-public class Cell extends Element{
-
-    public Cell(int x, int y) {
-        super(x, y);
-    }
-}
--- hero-solid-master/src/main/java/com/aor/hero/model/game/elements/Hero.java
+++
@@ -1,8 +0,0 @@
-package com.aor.hero.model.game.elements;
-
-public class Hero extends Element {
-
-    public Hero(int x, int y) {
-        super(x, y);
-    }
-}
